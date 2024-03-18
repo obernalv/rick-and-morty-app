e66781d3d48d2677d76840d326b2154175dc390f
Omar Bernal <obernalvillamar@gmail.com>
2024-03-18 13:19:08 -0500

    banner change and responsiveness correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,47 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import useHttp from './hooks/useHttp'
-import FormSearch from './components/FormSearch'
-import getRandomNumber from './services/getRandomNumber'
-import LocationInfo from './components/LocationInfo'
-import ResidentCard from './components/ResidentCard'
-import HasError from './components/HasError'
+import { useEffect, useState } from "react";
+import "./App.css";
+import useHttp from "./hooks/useHttp";
+import FormSearch from "./components/FormSearch";
+import getRandomNumber from "./services/getRandomNumber";
+import LocationInfo from "./components/LocationInfo";
+import ResidentCard from "./components/ResidentCard";
+import HasError from "./components/HasError";
 
 function App() {
+  const randomLocation = getRandomNumber(126);
+  const [locationSelected, setLocationSelected] = useState(randomLocation);
 
-  const randomLocation = getRandomNumber(126)
-  const [locationSelected, setLocationSelected] = useState(randomLocation)
-
-  const url = `https://rickandmortyapi.com/api/location/${locationSelected || getRandomNumber(126)}`
-  const [location, getLocation, hasError] = useHttp(url)
-
+  const url = `https://rickandmortyapi.com/api/location/${
+    locationSelected || getRandomNumber(126)
+  }`;
+  const [location, getLocation, hasError] = useHttp(url);
 
   useEffect(() => {
-    getLocation()   
-  }, [locationSelected])
-  
+    getLocation();
+  }, [locationSelected]);
 
   return (
+    <div className="app">
 
-    <div className='app'>
+      <img className="banner" src="/images/banner_rm2.jpg" alt="" />
 
-        <div className='container-banner'>
-          <img className='banner' src="/images/banner_rm.jpg" alt="" />
-        </div> 
-      
-        <FormSearch 
-          setLocationSelected ={setLocationSelected}
-        />
+      <FormSearch setLocationSelected={setLocationSelected} />
 
-        {hasError 
-          ? (<HasError />)
-          :(
-              <>
-                <LocationInfo 
-                    location = {location}
-                />
+      {hasError ? (
+        <HasError />
+      ) : (
+        <>
+          <LocationInfo location={location} />
 
-                <div className='container-resident'>
-                  {
-                    location?.residents.map(url =>(
-                      <ResidentCard 
-                        key={url}
-                        url = {url}
-                      />
-                    ))
-                  }
-                </div>
-
-
-              </>
-
-            )
-
-            
-        }
-        
-
+          <div className="container-resident">
+            {location?.residents.map((url) => (
+              <ResidentCard key={url} url={url} />
+            ))}
+          </div>
+        </>
+      )}
     </div>
-      
-
-  )
+  );
 }
 
-export default App
+export default App;
